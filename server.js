8c6481842325c1b81df07672ae44c14efbaa2d88
pradisa <pradisat.19msc@kongu.edu>
2025-02-24 17:16:31 +0530

    updated 1st commit

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const pool = require('./config/db');
 require('dotenv').config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // app.use('/',(req,res)=>{
 //     res.send("Hello World")
 // })
 app.use('/api',require('./routes/index'))
 const PORT = process.env.PORT || 5000
-console.log("Server Running.....")
+console.log("Server Running.....🚀 ")
 app.listen(PORT, () => console.log(`Server running on port ${PORT}....`))
